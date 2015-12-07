6c6f913d979ba37dd92a31c56a55baf3967a3815
Deric Bunch <ddbunc2@gmail.com>
2015-12-07 14:44:41 -0600

    able to retrieve data from nashville.gov via ajax call

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -2,6 +2,7 @@ define(function(require){
   var angular = require('angular');
   var ngRoute = require('ngRoute');
   var bootstrap = require('bootstrap');
+  var $ = require('jquery');
   angular
     .module('NashApp.home', ['ngRoute'])
     .config(["$routeProvider", function($routeProvider) {
@@ -12,6 +13,10 @@ define(function(require){
     }])
     .controller("HomeCtrl", ["$scope", "$location", 
       function($scope, $location) {
-      
+        $.ajax({
+          url: 'https://data.nashville.gov/resource/m4hn-ihe4?$$app_token=2HidFmxrQKRkdIzCsVe4bhqk9'
+        }).done(function(data) {
+          console.log(data);
+        });
     }]);
 });
