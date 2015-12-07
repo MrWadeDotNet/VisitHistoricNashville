ce3b86bb308f345348299f0e28420a1052978e1a
Deric Bunch <ddbunc2@gmail.com>
2015-12-07 15:24:49 -0600

    git basic data and print to page

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -2,7 +2,6 @@ define(function(require){
   var angular = require('angular');
   var ngRoute = require('ngRoute');
   var bootstrap = require('bootstrap');
-  var $ = require('jquery');
   angular
     .module('NashApp.home', ['ngRoute'])
     .config(["$routeProvider", function($routeProvider) {
@@ -11,12 +10,12 @@ define(function(require){
         controller: "HomeCtrl"
       });
     }])
-    .controller("HomeCtrl", ["$scope", "$location", 
-      function($scope, $location) {
-        $.ajax({
-          url: 'https://data.nashville.gov/resource/m4hn-ihe4?$$app_token=2HidFmxrQKRkdIzCsVe4bhqk9'
-        }).done(function(data) {
-          console.log(data);
+    .controller("HomeCtrl", ["$scope", "$location", "$http", 
+      function($scope, $location, $http) {
+        $http.get('https://data.nashville.gov/resource/m4hn-ihe4?$$app_token=2HidFmxrQKRkdIzCsVe4bhqk9')
+        .then(function(data){
+          console.log(data.data);
+          $scope.places = data.data;
         });
     }]);
 });
